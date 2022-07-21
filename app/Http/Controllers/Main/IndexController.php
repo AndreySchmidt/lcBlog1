b04f57f2a0f18e9d67de3bc82c6f014b004e95e5
AndreySchmidt <ringnull@gmail.com>
2022-07-21 17:44:22 +0300

    main controller + route

---
+++ app/Http/Controllers/Main/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Main;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        return 't';
+    }
+    
+    // public function index()
+    // {
+    //     return 'aaaa1';
+    // }
+}
