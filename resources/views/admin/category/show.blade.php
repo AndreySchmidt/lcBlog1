58b845c4b390cac7a0cbe30d4f03e01b9c93cf72
AndreySchmidt <ringnull@gmail.com>
2022-07-23 10:23:21 +0300

    admin caterory show

--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -90,46 +90,39 @@
         <div class="row">
         
         <div class="card col-6">
-        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить категорию</a>
-              <div class="card-header border-transparent">
-                <h3 class="card-title">Список категорий</h3>
+            <div class="card-header border-transparent">
+              <h3 class="card-title">{{ $category->title }}</h3>
 
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                  <i class="fas fa-times"></i>
+                </button>
               </div>
-              <!-- /.card-header -->
-              <div class="card-body p-0">
-                <div class="table-responsive">
-                  <table class="table m-0">
-                    <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Название</th>
-                      <th>Действие</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                      @foreach($categories as $category)
-                    <tr>
-                      <td>{{ $category->id }}</td>
-                      <td>{{ $category->title }}</td>
-                      <td><a href="{{ route('admin.category.show', $category->id) }}"><i class="far fa-eye"></i></a></td>
-                    </tr>
-                    @endforeach
-                    </tbody>
-                  </table>
-                </div>
-                <!-- /.table-responsive -->
-              </div>
-              <!-- /.card-body -->
-              <!-- /.card-footer -->
             </div>
+            <!-- /.card-header -->
+            <div class="card-body p-0">
+              <div class="table-responsive">
+                <table class="table m-0">
+                  <tbody>
+                  <tr>
+                    <td>ID</td>
+                    <td>{{ $category->id }}</td>
+                  </tr>
+                  <tr>
+                    <td>Title</td>
+                    <td>{{ $category->title }}</td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.table-responsive -->
+            </div>
+            <!-- /.card-body -->
+            <!-- /.card-footer -->
+          </div>
         </div>
         <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
